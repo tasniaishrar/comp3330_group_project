f2518a47e6ca09195c012af4d2721bac831711b8
Lee Yoon Jeong <yoonjeongelin16@gmail.com>
2022-11-11 12:57:54 +0800

    Update App.js

--- App.js
+++ App.js
@@ -1,9 +1,8 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import LoginScreen from './screens/login';
-import HomeScreen from './screens/home';
+import { StyleSheet } from 'react-native';
+import HomeScreen from './screens/HomeScreen';
+import LoginScreen from './screens/LoginScreen';
 
 const Stack = createNativeStackNavigator();
 
